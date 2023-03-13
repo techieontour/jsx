826c00342712895461ff755794c5439e687544a7
CIPL1304 <colanpmp@gmail.com>
2023-03-13 20:37:00 +0530

    inital

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Component } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: "Linda",
+        },
+        {
+          name: "Frank",
+        },
+        {
+          name: "Jacky",
+        },
+        {
+          name: "Lucy",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
